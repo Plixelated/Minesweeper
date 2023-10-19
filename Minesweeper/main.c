19063ba52d50ec5fc171825f16a1b97239e245fb
Plixelated <76276169+Plixelated@users.noreply.github.com>
2023-10-19 13:29:52 -0700

    Initial Commit

---
+++ Minesweeper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Minesweeper
+//
+//  Created by Mia Fraser on 10/19/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
